225e74b286666a554294a16a30b29cf5a729ce68
kush45 <30504408+kush45@users.noreply.github.com>
2021-09-21 07:05:54 +0530

    Update settings.py

--- Register/settings.py
+++ Register/settings.py
@@ -79,10 +79,10 @@ DATABASES = {
 'default': {
     'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'postgres',
-    'USER': 'postgres',
+    'USER': '',
     'PASSWORD': '',
     'HOST': '127.0.0.1',
-    'PORT': '5432',
+    'PORT': '',
 }
 }
 
