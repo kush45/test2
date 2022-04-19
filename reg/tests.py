27371fc0070d7659232ecf2b658147223b4c1335
kush45 <30504408+kush45@users.noreply.github.com>
2022-04-19 17:05:52 +0530

    Update tests.py

--- reg/tests.py
+++ reg/tests.py
@@ -2,3 +2,16 @@ from django.test import TestCase
 
 # Create your tests here.
 # Create your tests here.
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+
+# Create your tests here.from django.test import TestCase
+
+# Create your tests here.from django.test import TestCase
+
+# Create your tests here.
