285be564d5e0afb45cf198bd1bc40e4adf67fd1d
kush45 <kushmanohar45@gmail.com>
2022-04-19 17:09:02 +0530

    view file hasbeen added

--- reg/tests.py
+++ reg/tests.py
@@ -5,6 +5,27 @@ from django.test import TestCase
 
 # Create your tests here.
 from django.test import TestCase
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCasefrom django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+
+# Create your tests here.
+from django.test import TestCase
+
 
 # Create your tests here.
 from django.test import TestCase
